287d5c7f4aeeaf202ca9eae1c1de1bee28950bc7
Tiago R. Barcellos <tiagorb.vix@gmail.com>
2026-09-04 13:56:14 +0000

    Resolvendo exercícios de TAD

---
+++ 04_TAD_simples/TAD_01/Respostas/Tiago/tabuleiro.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include "tabuleiro.h"
+
+/**
+ * Cria um tabuleiro e retorna o tabuleiro criado.
+ * 
+ * @return o tabuleiro criado.
+ */
+tTabuleiro CriaTabuleiro() {
+
+    tTabuleiro tabuleiro;
+    int i, j;
+
+    for (i = 0; i < TAM_TABULEIRO; i++) {
+        for (j = 0; j < TAM_TABULEIRO; j++) {
+            tabuleiro.posicoes[i][j] = '-';
+        }
+    }
+}
+
+
+/**
+ * Marca uma posição do tabuleiro com a peça do jogador e retorna o tabuleiro.
+ * 
+ * @param tabuleiro o tabuleiro atual.
+ * @param peca a peça do jogador (1 ou 2).
+ * @param x a coordenada x da posição a ser marcada.
+ * @param y a coordenada y da posição a ser marcada.
+ * 
+ * @return o tabuleiro atualizado.
+ */
+tTabuleiro MarcaPosicaoTabuleiro(tTabuleiro tabuleiro, int peca, int x, int y) {
+
+    tabuleiro.posicoes[y][x] = peca;
+
+    return tabuleiro;
+}
+
+
+/**
+ * Verifica se há alguma posição livre no tabuleiro.
+ * 
+ * @param tabuleiro o tabuleiro atual.
+ * 
+ * @return 1 se há posição livre, 0 caso contrário.
+ */
+int TemPosicaoLivreTabuleiro(tTabuleiro tabuleiro) {
+    int i, j;
+
+    for (i = 0; i < TAM_TABULEIRO; i++) {
+        for (j = 0; j < TAM_TABULEIRO; j++) {
+            tabuleiro.posicoes[i][j] = '-';
+        }
+    }
+}
+
+
+/**
+ * Verifica se a posição do tabuleiro está marcada com a peça do jogador.
+ * 
+ * @param tabuleiro - o tabuleiro atual.
+ * @param x a coordenada x da posição a ser verificada.
+ * @param y a coordenada y da posição a ser verificada.
+ * @param peca a peça do jogador (1 ou 2).
+ * 
+ * @return 1 se a posição está marcada com a peça do jogador, 0 caso contrário.
+ */
+int EstaMarcadaPosicaoPecaTabuleiro(tTabuleiro tabuleiro, int x, int y, int peca);
+
+
+/**
+ * Verifica se a posição do tabuleiro está livre.
+ * 
+ * @param tabuleiro o tabuleiro atual.
+ * @param x a coordenada x da posição a ser verificada.
+ * @param y a coordenada y da posição a ser verificada.
+ * 
+ * @return 1 se a posição está livre, 0 caso contrário.
+ */
+int EstaLivrePosicaoTabuleiro(tTabuleiro tabuleiro, int x, int y);
+
+/**
+ * Verifica se a posição do tabuleiro é válida.
+ * 
+ * @param x a coordenada x da posição a ser verificada.
+ * @param y a coordenada y da posição a ser verificada.
+ * @return 1 se a posição é válida, 0 caso contrário.
+*/
+int EhPosicaoValidaTabuleiro(int x, int y);
+
+
+/**
+ * Verifica se o tabuleiro está cheio.
+ * 
+ * @param tabuleiro o tabuleiro atual.
+ */
+void ImprimeTabuleiro(tTabuleiro tabuleiro);
+
+#endif
